d6c753e92d5a534f8c9355b5d04d06262d079d06
Yanis <yanisariouapro@gmail.com>
2020-11-17 11:13:46 +0100

    Creation de la fonction NavigateBack

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<Page>
+    <Label text = "homepage" />
+    <Button text="Button" @tap="onButtonTap" />
+    
+    </Page>
+</template>
+
+<script>
+export default {
+    methods :{
+        onButtonTap(){
+            this.$navigateBack()
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
